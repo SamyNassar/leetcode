c208aa4ca4789a0585016c7210481d877f8baaf7
Samy Nassar <47665908+SamyNassar@users.noreply.github.com>
2023-01-28 21:01:18 +0200

    Time: 13 ms (62.49%), Space: 7.4 MB (17.61%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,9 +4,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        if(s.size() != t.size()) return false;        
+        unordered_map<char, int> counts;
         
-        return s==t ? true : false;
+        for(int i = 0; i < s.size(); i++){
+            counts[s[i]]++;
+            counts[t[i]]--;
+        }
+        for(auto c : counts){
+            if (c.second != 0) return false;
+        }
+        return true;
+        
     }
 };
