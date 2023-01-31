e4a12b932e5305202263584b47837344b6451ae7
Samy Nassar <47665908+SamyNassar@users.noreply.github.com>
2023-01-31 21:23:05 +0200

    Time: 50 ms (48.17%), Space: 21.3 MB (26.90%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string, vector<int>> anagrams;
+        vector<vector<string>> res;
+        
+        for(int i = 0; i < strs.size(); i++){
+            string curr = strs[i];
+            sort(curr.begin(), curr.end());
+            anagrams[curr].push_back(i);
+        }
+        
+        for(auto itr = anagrams.begin(); itr != anagrams.end(); itr++){
+            vector<int> v = itr->second;
+            vector<string> temp;
+            for(int i : v){
+                temp.push_back(strs[i]);
+            }
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
